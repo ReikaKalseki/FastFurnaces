ced65490d58746c5f3aeacd227ced9ecc9267524
Reika <reikasminecraft@gmail.com>
2017-07-02 01:47:36 -0400

    Update 4

--- config.lua
+++ config.lua
@@ -1,31 +1,31 @@
 Config = {}
 
 --This enables or disables the late-tier solars and steam engines to extend bobpower. Does nothing without that mod installed.
-Config.extendBobPower = true
+Config.extendBobPower = settings.startup["extend-bob-power"].value--true
 
 --This makes the Tier-5 productivity modules have no restrictions on their use.
-Config.freeUseModules = true
+Config.freeUseModules = settings.startup["free-use-modules"].value--true
 
 --Should the upper-tier modules be enabled?
-Config.modules = true
+Config.modules = settings.startup["bigger-modules"].value--true
 
 --Should the fast refineries be enabled?
-Config.fastRefinery = false
+Config.fastRefinery = settings.startup["fast-refinery"].value--false
 
 --Should the fast chemical plants be enabled?
-Config.fastChemical = false
+Config.fastChemical = settings.startup["fast-chemical"].value--false
 
 --Should the fast furnaces be enabled?
-Config.fastFurnace = false
+Config.fastFurnace = settings.startup["fast-furnace"].value--false
 
 --Should the fast beacons be enabled?
-Config.fastBeacon = true
+Config.fastBeacon = settings.startup["fast-beacon"].value--true
 
 --Should the fast drills be enabled?
-Config.fastDrill = true
+Config.fastDrill = settings.startup["fast-drill"].value--true
 
 --Should the fast pumps be enabled?
-Config.fastPump = true
+Config.fastPump = settings.startup["fast-pump"].value--true
 
 --Should the big chests be enabled?
-Config.bigChest = true
+Config.bigChest = settings.startup["bigger-chests"].value--true
